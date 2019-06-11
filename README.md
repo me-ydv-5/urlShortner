67315d8c153abec43acf836217c90cb7f0d8090d
Sahil Yadav <yadavsahil5198@gmail.com>
2019-06-11 18:49:43 +0530

    Added README and working demo GIF.

---
+++ README.md
@@ -0,0 +1,103 @@
+# URL Shortner
+
+A simple URL Shortner built using Node.js and Express.js as backend, an HTML frontend, and postgreSQL as database.
+
+## Introduction
+
+It's a simple application that takes a URL as input and outputs a substring of its hashed version (of the URL) as a shortened URL.
+
+The following endpoints are available
+
+    GET /<short_URL>  - Redirects user to the original URL, if it exists in the database else throws a 404 error if there isn't any record.
+
+    DELETE /<short_URL> - Deletes an existing short URL from the database if it exists, else throws a 404 error if there isn't any record.
+
+    POST /generate-short-url - Generates the hashed version of the long url. The POST parameters should be {url: <long_URL>}. Returns the short url as json response {short-url: <hashed_substring>} 
+
+    GET /list   - Retieves the list of all the records in the database. A record consists of the original URL, its shortened version and its hitrate, i.e, number of times the link has been clicked.
+ 
+The frontend has allows you to see current existing records, enter a new record and see the hitrates of the URLs. Also, you can delete any record directly from the frontend.
+
+It also has a rate limiter, which allows a maximum of 20 requests per second overall and throttles them if the number exceeds.
+
+See the following GIF that I made to see how it looks in action:
+
+![URL Shortner Example](demo.gif)
+
+---
+## Requirements
+
+* Node.js
+* Express.js
+* postgreSQL 
+
+### Node
+- #### Node installation on Ubuntu
+
+  You can install nodejs and npm easily with apt install, just run the following commands.
+
+      $ sudo apt install nodejs
+      $ sudo apt install npm
+
+    If the installation was successful, you should be able to run the following command.
+
+    ```
+    $ node --version
+    v12.4.0
+    
+    $ npm --version
+    6.9.0
+    ```
+
+
+    If you need to update `npm`, you can make it using `npm`!  After running the following command, just open again the command line and be happy.
+
+    `$ npm install npm -g`
+
+### postgreSQL
+Install postgreSQL on your machine.
+
+-   `sudo apt update`
+-   `sudo apt install postgresql postgresql-contrib`
+
+After executing the above two commands, create a new user for yourself by entering:
+
+-   `sudo -u postgres createuser --interactive`
+
+It will ask you to enter your *USERNAME* (for the most part it should be your machine's username) and if it needs to be superuser role. Enter your username and press y for making it a superuser.
+
+-   Create a database by typing `sudo -u USERNAME createdb DATABASENAME` to create a database.
+
+-   You may also need to set password for your new user.
+
+        $ psql
+        $ ALTER USER USERNAME WITH PASSWORD 'myPassword';
+        $ \q
+
+Note that USERNAME in the above commands is what you put your username as.
+
+---
+
+## Install
+
+    $ git clone https://github.com/me-ydv-5/urlShortner
+    $ cd urlShortner
+    $ psql DATABASENAME < urlshortner
+    $ npm install
+
+The last command imports the database from the dump.
+
+
+## Running the project
+
+Run the project with  the following command:
+
+    PGUSER='USERNAME' PGDATABASE='DATABASENAME' PGPASSWORD='USERPASSWORD' PGPORT='5432' nodemon index.js
+
+Where USERNAME is your username, DATABASENAME is the name of database you created in the above commands, USERPASSWORD is your account's password in postgres that you created.
+
+Go to `localhost:3000` and try out the app.
+
+If everthing goes well, you should be able to see the app frontend as well as send HTTP requests directly to the backend.
+
+**NOTE: If you have problems setting up the database, see [this](https://www.digitalocean.com/community/tutorials/how-to-install-and-use-postgresql-on-ubuntu-18-04)**
--- index.js
+++ index.js
@@ -1,124 +1,185 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const Bottleneck = require('bottleneck')
-const crypto = require('crypto')
-const bodyParser = require('body-parser')
-const Ddos = require('ddos')
-// A max of 20 requests running at a time.
-// Wait at least 1000ms between each request.
-const limiter = new Bottleneck({
- maxConcurrent: 20,
- minTime: 1000
-});
+const express = require('express');
+const app = express();
+const port = 3000;
+const crypto = require('crypto');
+const bodyParser = require('body-parser');
+const Ddos = require('ddos');
+const path = require("path");
+const rateLimit = require("express-rate-limit");
 
-const { Pool, Client } = require('pg')
+const limiter = rateLimit({
+	windowMs: 1000,
+	max: 20 // A max of 20 requests per second
+  });
 
-const pool = new Pool({
-	user:'sahil',
-	host:'localhost',
-	port:'5432',
-	database:'urlshortner',
-	password:'sahil'
-})
+const { Pool, Client } = require('pg');
 
-const client = new Client({
-	user:'sahil',
-	host:'localhost',
-	port:'5432',
-	database:'urlshortner',
-	password:'sahil'
-})
+// Create a client pool for Node-postgres connection.
+// A max of 20 clients could concurrently query the DB.
+// See more at https://node-postgres.com/features/pooling
+const pool = new Pool({max: 20});
 
-app.listen(port, () => console.log(`Listening on port ${port}!`))
-client.connect()
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 /*--------------------------------------------------------------------------*/
 app.use( bodyParser.json() );
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(limiter);
 
-var ddos = new Ddos({limit: 4, burst:4});
-app.use(ddos.express)
+// It will start denying requests from a given IP if there are more than
+// 4 requests per second. If there aren't any more, it will reset the counter (of 
+// requests made) to 0. If the client exceeds, the time to reset counter doubles. 
+// See more at: 
+// https://www.npmjs.com/package/ddos#how-does-this-ddos-prevention-module-work
+var ddos = new Ddos({limit: 20, burst:4});
+app.use(ddos.express);
 
-app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname + 'public/index.html'))
-})
+// Homepage
+app.get('/', (req, res, next) => {
+	res.sendFile(path.join(__dirname + 'public/index.html'));
+});
 
-
+// Retrieves all the records from the database
 app.get('/list', (req, response)=>{
-	var query = 'SELECT * from url'
-	client.query(query)
-  		.then(res => {
-  			response.json(res.rows)
-  		})
-  		.catch(e => console.error(e.stack))
-})
+	var query = 'SELECT * from url';
+	
+	// pool.connect returns a client from the pool
+	// and then we query using that client.
+	// Once done, the client is freed so that we can use it
+	// for some another query.
+	pool.connect()
+		.then(client => {
+			return client.query(query)
+			.then(res => {
+				response.json(res.rows);
+				client.release();
+			})
+			.catch(e => {
+				client.release();
+				console.error(e.stack);
+			})
+		})
+		.catch(error => console.log(error));
+});
 
-app.post('/generate-short-url', (req, res) => {
-	var url = req.body.url
+// Generates the shortened URL by calculating the MD5 hash of the longURL
+// and taking the first 6 characters of the hash.
+app.post('/generate-short-url', (req, res, next) => {
+	var url = req.body.url;
 
 	var hash = crypto.createHash('md5').update(url).digest('hex');
-	hash = hash.substring(0, 6)
+	hash = hash.substring(0, 6);
 
-	var qry = 'INSERT INTO url VALUES ($1, $2, 0) ON CONFLICT (shorturl) DO NOTHING;'
-	var values = [hash, url]
+	var qry = 'INSERT INTO url VALUES ($1, $2, 0) ON CONFLICT (shorturl) DO NOTHING;';
+	var values = [hash, url];
 
-	client.query(qry, values)
-		.then((response) => {
-			res.json({'short-url': hash})
+	pool.connect()
+		.then(client => {
+			return client.query(qry, values)
+			.then((response) => {
+				res.json({'short-url': hash});
+				client.release();
+			})
+			.catch(e => {
+				client.release();
+				console.error(e.stack)
+			});	
 		})
-		.catch(e => console.error(e.stack))
-})
+		.catch(error => console.log(error));
+});
 
-app.get('/:shorturl', (req,res) => {
-	var shorturl = req.params['shorturl']
-	
-	// var ip = req.connection.remoteAddress
-	// console.log(ip)
+// Gets the the longURL for a particular hash, if exists and redirects.
+// If no hash is present, returns 404 with error page.
+app.get('/:shorturl', (req,res, next) => {
+	var shorturl = req.params['shorturl'];
+	var query = 'SELECT longurl FROM url where shorturl=$1;';
+	var values = [shorturl];
 
-	var query = 'SELECT longurl FROM url where shorturl=$1;'
-	var values = [shorturl]
+	pool.connect()
+	.then(client => {
+		client.query(query, values)
+			.then(response => {
+				if(response.rowCount == 1){
+					var longurl = response.rows[0]['longurl'];
 
-	client.query(query, values)
-		.then(response => {
-			if(response.rowCount == 1){
-				var longurl = response.rows[0]['longurl']
+					// Add http:// protocol prefix is there isn't one
+					// This is so as not to let the browser think it is
+					// one of the suburls.
+					if(longurl.substring(0, 4) != "http")
+						longurl = "http://" + longurl;
+					res.redirect(longurl);
 
-				if(longurl.substring(0, 4) != "http")
-					longurl = "http://" + longurl
-				res.redirect(longurl)
+					// Update the hitrate for the link if it has been found.
+					var counterQuery = 'UPDATE url SET hitrate=hitrate+1 '
+									+ 'where shorturl = $1;';
+					var values = [shorturl];
 
-				var counterQuery = 'UPDATE url SET hitrate=hitrate+1 '
-								 + 'where shorturl = $1;'
-				var values = [shorturl]
-				client.query(counterQuery, values, (err, res) => {
-				  	if (err) {
-				    	console.log(err.stack)
-				  	} else {
-				  		console.log("Updated the hitrate")
-				  	}
-				})
-			}else{
-				res.sendStatus(404)
-			}
+					pool.connect()
+						.then(client => {
+							return client.query(counterQuery, values, (err, res) => {
+								if (err) {
+									console.log(err.stack);
+								} else {
+									console.log("Updated the hitrate");
+								}
+
+								client.release();
+							});
+						})
+				}else{
+					// Error handler.
+					next("There's nothing here!");
+				}
+			})
+			.catch(e => {
+				console.error(e.stack)
+			});
+	})
+});
+
+// Delete a given shortURL, if exists.
+app.delete('/:shorturl', (req, res, next) => {
+	var shorturl = req.params['shorturl'];
+	var values = [shorturl];
+
+	var query = 'DELETE FROM url WHERE shorturl=$1;';
+	var values = [shorturl];
+
+	pool.connect()
+		.then(client => {
+			return client.query(query, values)
+			.then(response => {
+				if(response.rowCount == 0){
+					res.sendStatus(404);
+				}else{
+					res.send('Deleted!');
+				}
+				client.release();
+			})
+			.catch(e => {
+				client.release();
+				console.error(e.stack)
+			});
 		})
-		.catch(e => console.error(e.stack))
-})
+});
 
-app.delete('/:shorturl', (req, res) => {
-	var shorturl = req.params['shorturl']
-	var values = [shorturl]
+// Default error handler.
+app.use(function(err, req, res, next){
+	res.status(404);
 
-	var query = 'DELETE FROM url WHERE shorturl=$1;'
-	var values = [shorturl]
+	// respond with html page
+	if (req.accepts('html')) {
+		res.sendFile(path.join(__dirname + '/error.html'));
+	  	return;
+	}
 
-	client.query(query, values)
-		.then(response => {
-			if(response.rowCount == 0){
-				res.sendStatus(404)
-			}else{
-				res.send('Deleted!')
-			}
-		})
-		.catch(e => console.error(e.stack))
-})
+	// respond with json
+	if (req.accepts('json')) {
+	  res.send({ error: err });
+	  return;
+	}
+
+	// default to plain-text. send()
+	res.type('txt').send(err);
+  });
